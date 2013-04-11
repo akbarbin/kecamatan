906595a77a285a2df27bd1f5d7df1c3275860854
Muhamad Akbar Bin Widayat <muhamad.akbar@kiranatama.com>
2013-04-11 22:42:12 +0700

    rails apps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kecamatan::Application.config.secret_token = '0efcd398ae0b6c8a78294b5b2b25697d3b3311cec3e9521490279d0a341ee13150619163178104be4ebb758b8789adfac7dbf74f2c38403034532cca56ceabea'

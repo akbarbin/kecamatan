3980e317ad3276d35ac24b1b546e9e869d195466
Muhamad Akbar Bin Widayat <muhamad.akbar@kiranatama.com>
2013-05-13 21:40:29 +0700

    import excel data master tabular

--- app/models/export.rb
+++ app/models/export.rb
@@ -28,10 +28,34 @@ class Export
       columns = data.row(i+7)
       next if columns[0].blank?
       ancestry = Export.get_space(columns[0], id)
-#      MasterTabular.create(
-#        name: columns[0],
-#        ancestry: ancestry
-#      )
+      MasterTabular.create(
+        name: columns[0],
+        ancestry: ancestry
+      )
+    end
+    FileUtils.rm(excel) if remove_after
+  end
+
+  # Created by [muhamadakbarbw@gmail.com] at April 16 2013,
+  # Import dummy data
+  def self.import(excel, remove_after = false)
+    workbook = Spreadsheet.open(excel)
+    data = workbook.worksheet 0
+    rows = data.row_count - 1
+    parent_id = []
+    parent_id[0] = nil
+    rows.times do |i|
+      columns = data.row(i+2)
+      4.times do |j|
+        if columns[j+1].present?
+          tabular = MasterTabular.create(
+            name: columns[j+1],
+            parent_id: parent_id[j]
+          )
+          parent_id[j+1] = tabular.id
+          break
+        end
+      end
     end
     FileUtils.rm(excel) if remove_after
   end
@@ -42,12 +66,12 @@ class Export
     ancestry = ""
     size = name.scan(/\s/).size
     puts size
-#    case size
-#    when 7
-#      ancestry = id
-#    else
-#      ancestry = 1
-#    end
-#    ancestry
+    #    case size
+    #    when 7
+    #      ancestry = id
+    #    else
+    #      ancestry = 1
+    #    end
+    #    ancestry
   end
 end
